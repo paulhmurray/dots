19e6e5fa4c09a9abcdf221a7c47581c7c7150838
Paul <paulhughmurray@gmail.com>
2026-09-15 22:03:31 +1000

    Treesitter, LSP for Go and Lua

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -15,6 +15,7 @@ o.undofile = true
 o.scrolloff = 8
 o.splitright = true
 o.splitbelow = true
+o.completeopt = { "menu", "menuone", "noselect" }
 
 -- lazy.nvim bootstrap
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -24,12 +25,47 @@ if not vim.uv.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+local langs = { "lua", "go", "gomod", "bash", "dart", "markdown", "json", "yaml" }
+
 require("lazy").setup({
   { "catppuccin/nvim", name = "catppuccin", priority = 1000,
     config = function() vim.cmd.colorscheme("catppuccin-mocha") end },
   { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+  { "nvim-treesitter/nvim-treesitter", branch = "main", lazy = false, build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter").install(langs)
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = langs,
+        callback = function() vim.treesitter.start() end,
+      })
+    end },
+  { "neovim/nvim-lspconfig" },
 })
 
+-- LSP
+vim.lsp.enable({ "gopls", "lua_ls" })
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if client and client:supports_method("textDocument/completion") then
+      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+    end
+    local map = function(keys, fn) vim.keymap.set("n", keys, fn, { buffer = ev.buf }) end
+    map("gd", vim.lsp.buf.definition)
+    map("gr", vim.lsp.buf.references)
+    map("<leader>rn", vim.lsp.buf.rename)
+    map("<leader>ca", vim.lsp.buf.code_action)
+  end,
+})
+
+-- format Go on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function() vim.lsp.buf.format({ async = false }) end,
+})
+
+-- keymaps
 local map = vim.keymap.set
 map("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
 map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
